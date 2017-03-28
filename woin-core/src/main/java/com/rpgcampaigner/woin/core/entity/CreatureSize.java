47245aa96780cf6a697b1af9f6f8b0efb9602771
John McCormick <jmccormick@blizzard.com>
2017-03-28 09:40:03 -0700

    size and sentience enums with functions for default attributes

--- woin-core/src/main/java/com/rpgcampaigner/woin/core/entity/CreatureSize.java
+++ woin-core/src/main/java/com/rpgcampaigner/woin/core/entity/CreatureSize.java
@@ -1,5 +1,7 @@
 package com.rpgcampaigner.woin.core.entity;
 
+import java.util.function.Function;
+
 /**
  * @author jmccormick
  * @since 2/24/17
@@ -11,5 +13,67 @@ public enum CreatureSize {
 	LARGE,
 	ENORMOUS,
 	GIGANTIC,
-	COLOSSAL;
+	COLOSSAL,
+	TITANIC;
+
+	Function<CreatureSize, Integer> defaultStrength = size -> {
+		switch (size) {
+			case TINY:
+				return 1;
+			case SMALL:
+				return 3;
+			case MEDIUM:
+				return 6;
+			case LARGE:
+				return 10;
+			case ENORMOUS:
+				return 20;
+			case GIGANTIC:
+				return 50;
+			case COLOSSAL:
+				return 80;
+			case TITANIC:
+				return 120;
+				default:
+					throw new IllegalArgumentException("Unsupported CreatureSize: " + size.name());
+		}
+	};
+
+	Function<CreatureSize, Integer> defaultAgility = size -> {
+		switch (size) {
+			case TINY:
+				return 20;
+			case SMALL:
+				return 10;
+			case MEDIUM:
+				return 6;
+			case LARGE: ENORMOUS: GIGANTIC: COLOSSAL: TITANIC:
+				return 4;
+			default:
+				throw new IllegalArgumentException("Unsupported CreatureSize: " + size.name());
+		}
+	};
+
+	Function<CreatureSize, Integer> defaultEndurance = size -> {
+		switch (size) {
+			case TINY:
+				return 1;
+			case SMALL:
+				return 3;
+			case MEDIUM:
+				return 6;
+			case LARGE:
+				return 10;
+			case ENORMOUS:
+				return 25;
+			case GIGANTIC:
+				return 60;
+			case COLOSSAL:
+				return 100;
+			case TITANIC:
+				return 200;
+			default:
+				throw new IllegalArgumentException("Unsupported CreatureSize: " + size.name());
+		}
+	};
 }
