3c5da63aa8af5133e1302585d968251a0f48a329
John McCormick <jmccormick@blizzard.com>
2017-05-15 08:22:32 -0700

    initial stub of StarSystemReference Service application

---
+++ woin-starSystemReferenceData-service/src/main/java/com/rpgcampaigner/woin/starSystemReference/Application.java
@@ -0,0 +1,20 @@
+package com.rpgcampaigner.woin.starSystemReference;
+
+import io.advantageous.qbit.server.EndpointServerBuilder;
+import io.advantageous.qbit.server.ServiceEndpointServer;
+
+/**
+ * @author jmccormick
+ * @since 5/15/17
+ */
+public class Application {
+	public static void main(String[] args) throws Exception {
+//		DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration("localhost", "woin-reference");
+//		ReferenceRepository referenceRepository = new ReferenceRepository(databaseConfiguration.getReferenceBucket());
+//		ReferenceManager referenceManager = new ReferenceManager(referenceRepository);
+//		ServiceEndpointServer server = new EndpointServerBuilder().build();
+//		server.initServices(new SkillService(referenceManager, referenceRepository));
+//		server.start();
+		System.out.println("Hello");
+	}
+}
